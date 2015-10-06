1492ac95aa285d88a18eb01ef742253e19668de1
Ka-Hing Cheung <khc@hxbc.us>
2015-10-05 22:28:38 -0700

    update test

--- goofys_test.go
+++ goofys_test.go
@@ -354,7 +354,7 @@ func (s *GoofysTest) TestReadDir(t *C) {
 	dh := s.getRoot(t).OpenDir()
 	defer dh.CloseDir()
 
-	s.assertEntries(t, s.getRoot(t), []string{ "dir1", "dir2", "empty_dir", "file1", "file2" })
+	s.assertEntries(t, s.getRoot(t), []string{ "dir1", "dir2", "empty_dir", "file1", "file2", "zero" })
 
 	// test listing dir1/
 	in, err := s.LookUpInode(t, "dir1")
