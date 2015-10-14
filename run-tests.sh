8860a7c13148cb2a7843631efe9ed1a6e28b8c88
Ka-Hing Cheung <khc@hxbc.us>
2015-10-14 13:57:28 -0700

    wait for proxy to start before running test

--- run-tests.sh
+++ run-tests.sh
@@ -12,9 +12,29 @@ function cleanup {
 
 trap cleanup EXIT
 
+function wait_for_proxy
+{
+
+    for i in $(seq 30);
+    do
+        if exec 3<>"/dev/tcp/localhost/8080";
+        then
+            exec 3<&-  # Close for read
+            exec 3>&-  # Close for write
+            return
+        fi
+        sleep 1
+    done
+
+    # we didn't start correctly
+    exit
+}
+
 PROXY_BIN="java -jar s3proxy.jar --properties s3proxy.properties"
 stdbuf -oL -eL $PROXY_BIN &
 PROXY_PID=$!
+
+wait_for_proxy
 
 go test -v ./... #-check.f TestUnlink
 exit $?
