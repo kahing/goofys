5668fd62183d592b9f39aca0868856b1f0f5a280
Ka-Hing Cheung <khc@hxbc.us>
2015-10-16 16:21:13 -0700

    forgot to uncomment this

--- bench.sh
+++ bench.sh
@@ -21,76 +21,76 @@ function create_files {
     done
 }
 
-# echo "Running create_files"
-# time create_files
+echo "Running create_files"
+time create_files
 
-# function ls_files {
-#     ls -1 | wc -l
-# }
+function ls_files {
+    ls -1 | wc -l
+}
 
-# echo "Running ls_files"
-# drop_cache
-# sleep 1
-# time ls_files
+echo "Running ls_files"
+drop_cache
+sleep 1
+time ls_files
 
-# drop_cache
-# sleep 1
-# time ls_files
+drop_cache
+sleep 1
+time ls_files
 
-# drop_cache
-# sleep 1
-# time ls_files
+drop_cache
+sleep 1
+time ls_files
 
-# function rm_files {
-#     for i in $(seq 1 1000); do
-#         rm file$i
-#     done
-# }
+function rm_files {
+    for i in $(seq 1 1000); do
+        rm file$i
+    done
+}
 
-# echo "Running rm_files/create_files"
-# drop_cache
-# time rm_files
+echo "Running rm_files/create_files"
+drop_cache
+time rm_files
 
-# drop_cache
-# time create_files
-# drop_cache
-# time rm_files
+drop_cache
+time create_files
+drop_cache
+time rm_files
 
-# drop_cache
-# time create_files
-# drop_cache
-# time rm_files
+drop_cache
+time create_files
+drop_cache
+time rm_files
 
-# function create_files_parallel {
-#     for i in $(seq 1 1000); do
-#         echo $i > file$i & true
-#     done
-#     wait
-# }
+function create_files_parallel {
+    for i in $(seq 1 1000); do
+        echo $i > file$i & true
+    done
+    wait
+}
 
-# function rm_files_parallel {
-#     for i in $(seq 1 1000); do
-#         rm file$i & true
-#     done
-#     wait
-# }
+function rm_files_parallel {
+    for i in $(seq 1 1000); do
+        rm file$i & true
+    done
+    wait
+}
 
-# echo "Running create_files_parallel/rm_files_parallel"
+echo "Running create_files_parallel/rm_files_parallel"
 
-# drop_cache
-# time create_files_parallel
-# drop_cache
-# time rm_files_parallel
+drop_cache
+time create_files_parallel
+drop_cache
+time rm_files_parallel
 
-# drop_cache
-# time create_files_parallel
-# drop_cache
-# time rm_files_parallel
+drop_cache
+time create_files_parallel
+drop_cache
+time rm_files_parallel
 
-# drop_cache
-# time create_files_parallel
-# drop_cache
-# time rm_files_parallel
+drop_cache
+time create_files_parallel
+drop_cache
+time rm_files_parallel
 
 function write_large_file {
     dd if=/dev/zero of=largefile bs=1MB count=1000
