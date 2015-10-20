0a5fcfb706ad67d6b2263bf3c2ca6870bfd0e730
Ka-Hing Cheung <khc@hxbc.us>
2015-10-20 16:25:17 -0700

    add benchmark for riofs

--- bench/run_bench.sh
+++ bench/run_bench.sh
@@ -9,6 +9,7 @@ mkdir bench-mnt
 $dir/bench.sh cat bench-mnt |& tee bench.local
 $dir/bench.sh "goofys --stat-cache-ttl 0 --type-cache-ttl 0 goofys bench-mnt" bench-mnt |& tee bench.goofys
 $dir/bench.sh "s3fs -ostat_cache_expire=1 -ourl=https://s3.amazonaws.com -f goofys bench-mnt" bench-mnt |& tee bench.s3fs
+$dir/bench.sh "riofs -f -c riofs.conf.xml goofys bench-mnt" bench-mnt |& tee bench.riofs
 rmdir bench-mnt
 
 $dir/format_bench.sh bench.s3fs bench.goofys
