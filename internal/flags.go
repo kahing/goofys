f68186552fb60bb266a2aaab049f8ffcc496f3b3
Ka-Hing Cheung <khc@hxbc.us>
2016-02-26 22:21:58 -0800

    v0.0.5

--- internal/flags.go
+++ internal/flags.go
@@ -54,7 +54,7 @@ func NewApp() (app *cli.App) {
 
 	app = &cli.App{
 		Name:     "goofys",
-		Version:  "0.0.4",
+		Version:  "0.0.5",
 		Usage:    "Mount an S3 bucket locally",
 		HideHelp: true,
 		Writer:   os.Stderr,
