d0d33aee89da1be6344e4bc2ffac45f6c7d2b183
Bryce Lampe <blampe@uber.com>
2016-06-25 12:51:49 -0700

    Make run-tests.sh work with OS X

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#set -o xtrace
+set -o xtrace
 set -o errexit
 set -o nounset
 
 function cleanup {
     if [ "$PROXY_PID" != "" ]; then
-        kill $PROXY_PID
+        kill "$PROXY_PID"
     fi
 }
 
@@ -22,7 +22,7 @@ mkdir -p /tmp/s3proxy
 
 export LOG_LEVEL=warn
 PROXY_BIN="java -jar s3proxy.jar --properties test/s3proxy.properties"
-stdbuf -oL -eL $PROXY_BIN &
+$PROXY_BIN >/dev/null 2>/dev/null &
 PROXY_PID=$!
 
 go test -timeout 20m -v $(go list ./... | grep -v /vendor/) $T
