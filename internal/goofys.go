kahing/goofys

commit 2ff8fcfe69d130ed3ec258d695dd391f46c691f3

beginning of minio support
test doesn't pass yet because minio doesn't allow blobs that end with /:
https://github.com/minio/minio/issues/1616

additionally goofys doesn't behave correctly when the bucket doesn't
exist because of https://github.com/minio/minio/issues/2496

refs #110

internal/goofys.go
@@ -139,16 +139,36 @@ func NewGoofys(bucket string, awsConfig *aws.Config, flags *FlagStorage) *Goofys
 		log.Errorf("bucket %v does not exist", fs.bucket)
 		return nil
 	} else {
-		// this is NOT AWS, maybe they don't support v4 signing
-		// swift3, ceph-s3 return 400; GCS, EMC return 403
-		fs.fallbackV2Signer()
+		// minio requires the region to be us-east-1
+		if *fs.awsConfig.Region != "us-east-1" {
+			fs.awsConfig.Region = aws.String("us-east-1")
+			fs.sess = session.New(awsConfig)
+			fs.s3 = fs.newS3()
+			s3Log.Infof("Unable to detect bucket region, using '%v'", *fs.awsConfig.Region)
+		}
+		// this is NOT AWS, we expect the request to fail with 403 if this is not
+		// an anonymous bucket, or if the provider doesn't support v4 signing, or both
+		// swift3 and ceph-s3 return 400 so we know we can fallback to v2 signing
+		// EMC returns 403 because it doesn't support v4 signing
+		// minio returns 403 because we are using anonymous credential
+		if err != syscall.EACCES {
+			fs.fallbackV2Signer()
+		}
 	}
 
-	// try again to make sure
-	err = fs.testBucket()
+	// try again with the credential to make sure
+	err = mapAwsError(fs.testBucket())
 	if err != nil {
-		log.Errorf("Unable to access '%v': %v", fs.bucket, mapAwsError(err))
-		return nil
+		if err == syscall.EACCES {
+			// if we still get EACCES, this could be EMC and we should try again
+			fs.fallbackV2Signer()
+			err = mapAwsError(fs.testBucket())
+		}
+
+		if err != nil {
+			log.Errorf("Unable to access '%v': %v", fs.bucket, err)
+			return nil
+		}
 	}
 
 	go fs.cleanUpOldMPU()
@@ -260,16 +280,16 @@ func (fs *Goofys) testBucket() (err error) {
 }
 
 func (fs *Goofys) detectBucketLocationByHEAD() (err error) {
-	config := &aws.Config{
-		Credentials: credentials.AnonymousCredentials,
-		Endpoint:    fs.awsConfig.Endpoint,
-		// always probe with us-east-1 region, otherwise the behavior of other endpoints
-		// maybe different
-		Region:   aws.String("us-east-1"),
-		Logger:   GetLogger("s3"),
-		LogLevel: aws.LogLevel(aws.LogDebug | aws.LogDebugWithRequestErrors),
-	}
-	sess := session.New(config)
+	config := *fs.awsConfig
+	config.Credentials = credentials.AnonymousCredentials
+	config.LogLevel = aws.LogLevel(aws.LogDebug | aws.LogDebugWithRequestErrors)
+	// always probe with us-east-1 region, otherwise the behavior of other endpoints
+	// maybe different
+	config.Region = aws.String("us-east-1")
+	// logger might not have been defined if we ran this in test
+	config.Logger = GetLogger("s3")
+
+	sess := session.New(&config)
 	tmpS3 := s3.New(sess)
 
 	req, _ := tmpS3.HeadBucketRequest(&s3.HeadBucketInput{Bucket: &fs.bucket})
@@ -292,8 +312,7 @@ func (fs *Goofys) detectBucketLocationByHEAD() (err error) {
 			fs.awsConfig.Region = &region[0]
 		}
 	} else {
-		s3Log.Infof("Unable to detect bucket region, staying at '%v'", *fs.awsConfig.Region)
-		err = fuse.EINVAL
+		err = mapAwsError(reqerr)
 	}
 	return
 }
@@ -370,6 +389,10 @@ func (fs *Goofys) GetInodeAttributes(
 }
 
 func mapAwsError(err error) error {
+	if err == nil {
+		return nil
+	}
+
 	if awsErr, ok := err.(awserr.Error); ok {
 		if reqErr, ok := err.(awserr.RequestFailure); ok {
 			// A service error occurred
@@ -883,6 +906,10 @@ func (fs *Goofys) FlushFile(
 		fs.mu.Lock()
 		fs.inodesCache[*fh.inode.FullName] = fh.inode
 		fs.mu.Unlock()
+	} else {
+		// if we returned success from creat() earlier
+		// linux may think this file exists even when it doesn't
+		// TODO: figure out a way to make the kernel forget this inode
 	}
 
 	return

internal/goofys_test.go
@@ -103,7 +103,7 @@ func logOutput(t *C, tag string, r io.ReadCloser) {
 	}
 }
 
-func (s *GoofysTest) waitFor(t *C, addr string) (err error) {
+func waitFor(t *C, addr string) (err error) {
 	// wait for it to listen on port
 	for i := 0; i < 10; i++ {
 		var conn net.Conn
@@ -121,17 +121,31 @@ func (s *GoofysTest) waitFor(t *C, addr string) (err error) {
 	return
 }
 
-func (s *GoofysTest) SetUpSuite(t *C) {
-	//addr := "play.minio.io:9000"
-	const LOCAL_TEST = true
-
-	if LOCAL_TEST {
+func selectTestConfig(t *C) *aws.Config {
+	const testAws = false
+	if testAws {
+		return &aws.Config{
+			Region:     aws.String("us-west-2"),
+			DisableSSL: aws.Bool(true),
+			//LogLevel:         aws.LogLevel(aws.LogDebug | aws.LogDebugWithSigning),
+			S3ForcePathStyle: aws.Bool(true),
+		}
+	} else if hasEnv("MINIO") {
+		return &aws.Config{
+			Credentials: credentials.NewStaticCredentials("Q3AM3UQ867SPQQA43P2F",
+				"zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG", ""),
+			Region: aws.String("us-east-1"),
+			//LogLevel:         aws.LogLevel(aws.LogDebug | aws.LogDebugWithSigning),
+			S3ForcePathStyle: aws.Bool(true),
+			Endpoint:         aws.String("play.minio.io:9000"),
+		}
+	} else {
 		addr := "127.0.0.1:8080"
 
-		err := s.waitFor(t, addr)
+		err := waitFor(t, addr)
 		t.Assert(err, IsNil)
 
-		s.awsConfig = &aws.Config{
+		return &aws.Config{
 			//Credentials: credentials.AnonymousCredentials,
 			Credentials:      credentials.NewStaticCredentials("foo", "bar", ""),
 			Region:           aws.String("us-west-2"),
@@ -143,19 +157,19 @@ func (s *GoofysTest) SetUpSuite(t *C) {
 			//LogLevel: aws.LogLevel(aws.LogDebug),
 			//LogLevel: aws.LogLevel(aws.LogDebug | aws.LogDebugWithHTTPBody),
 		}
-	} else {
-		s.awsConfig = &aws.Config{
-			Region:     aws.String("us-west-2"),
-			DisableSSL: aws.Bool(true),
-			//LogLevel:         aws.LogLevel(aws.LogDebug | aws.LogDebugWithSigning),
-			S3ForcePathStyle: aws.Bool(true),
-		}
 	}
+}
+
+func (s *GoofysTest) SetUpSuite(t *C) {
+	s.awsConfig = selectTestConfig(t)
+
 	s.sess = session.New(s.awsConfig)
 	s.s3 = s3.New(s.sess)
-	s.s3.Handlers.Sign.Clear()
-	s.s3.Handlers.Sign.PushBack(SignV2)
-	s.s3.Handlers.Sign.PushBackNamed(corehandlers.BuildContentLengthHandler)
+	if !hasEnv("MINIO") {
+		s.s3.Handlers.Sign.Clear()
+		s.s3.Handlers.Sign.PushBack(SignV2)
+		s.s3.Handlers.Sign.PushBackNamed(corehandlers.BuildContentLengthHandler)
+	}
 	_, err := s.s3.ListBuckets(nil)
 	t.Assert(err, IsNil)
 }
@@ -283,6 +297,9 @@ func (s *GoofysTest) TestLookUpInode(t *C) {
 	t.Assert(err, Equals, fuse.ENOENT)
 
 	_, err = s.LookUpInode(t, "dir1/file3")
+	t.Assert(err, IsNil)
+
+	_, err = s.LookUpInode(t, "dir2/dir3")
 	t.Assert(err, IsNil)
 
 	_, err = s.LookUpInode(t, "dir2/dir3/file4")
@@ -731,14 +748,19 @@ func (s *GoofysTest) TestConcurrentRefDeref(t *C) {
 	}
 }
 
-func isTravis() bool {
+func hasEnv(env string) bool {
+	prefix := env + "="
 	for _, e := range os.Environ() {
-		if strings.HasPrefix(e, "TRAVIS=") {
+		if strings.HasPrefix(e, prefix) {
 			return true
 		}
 	}
 
 	return false
+}
+
+func isTravis() bool {
+	return hasEnv("TRAVIS")
 }
 
 func (s *GoofysTest) mount(t *C, mountPoint string) {

commit c7fde8ec56fe40c1402029b56b9d9f27ac801dbd

test bucket using domain style requests
fixes a regression introduced by
2ff8fcfe69d130ed3ec258d695dd391f46c691f3

fixes #113

internal/goofys.go
@@ -283,6 +283,7 @@ func (fs *Goofys) detectBucketLocationByHEAD() (err error) {
 	config := *fs.awsConfig
 	config.Credentials = credentials.AnonymousCredentials
 	config.LogLevel = aws.LogLevel(aws.LogDebug | aws.LogDebugWithRequestErrors)
+	config.S3ForcePathStyle = aws.Bool(false)
 	// always probe with us-east-1 region, otherwise the behavior of other endpoints
 	// maybe different
 	config.Region = aws.String("us-east-1")