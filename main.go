afa820bcac11e4a959e5e608adf6aa1b9d50028e
Michael Smith <michael.smith@usace.army.mil>
2017-11-12 07:55:43 -0500

    change internal git repo

--- main.go
+++ main.go
@@ -16,8 +16,8 @@
 package main
 
 import (
-	goofys "github.com/kahing/goofys/api"
-	. "github.com/kahing/goofys/internal"
+	goofys "github.com/msmitherc/goofys/api"
+	. "github.com/msmitherdc/goofys/internal"
 
 	"fmt"
 	"os"
