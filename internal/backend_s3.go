a9faab1bd9867ad4a8927f171f7cd74c533ab8a2
Ka-Hing Cheung <kahing.cheung@databricks.com>
2019-05-09 15:30:02 -0700

    converted ListObjectsV2

--- internal/backend_s3.go
+++ internal/backend_s3.go
@@ -92,12 +92,19 @@ func (s *S3Backend) HeadBlob(param *HeadBlobInput) (*HeadBlobOutput, error) {
 	}, nil
 }
 
-func (s *S3Backend) ListBlobs(param *ListBlobInput) (*ListBlobOutput, error) {
+func (s *S3Backend) ListBlobs(param *ListBlobsInput) (*ListBlobsOutput, error) {
+	var maxKeys *int64
+
+	if param.MaxKeys != nil {
+		maxKeys = aws.Int64(int64(*param.MaxKeys))
+	}
+
 	resp, err := s.ListObjectsV2(&s3.ListObjectsV2Input{
 		Bucket:            &s.fs.bucket,
-		Prefix:            &param.Prefix,
-		Delimiter:         &param.Delimiter,
-		MaxKeys:           aws.Int64(int64(param.MaxKeys)),
+		Prefix:            param.Prefix,
+		Delimiter:         param.Delimiter,
+		MaxKeys:           maxKeys,
+		StartAfter:        param.StartAfter,
 		ContinuationToken: param.ContinuationToken,
 	})
 	if err != nil {
@@ -120,9 +127,8 @@ func (s *S3Backend) ListBlobs(param *ListBlobInput) (*ListBlobOutput, error) {
 		})
 	}
 
-	return &ListBlobOutput{
+	return &ListBlobsOutput{
 		ContinuationToken:     param.ContinuationToken,
-		MaxKeys:               param.MaxKeys,
 		Prefixes:              prefixes,
 		Items:                 items,
 		NextContinuationToken: resp.NextContinuationToken,
