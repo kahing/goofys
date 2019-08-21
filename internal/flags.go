42c567cafc27c7f68086f9709a19f03b56e2e80e
Ka-Hing Cheung <kahing.cheung@databricks.com>
2019-08-21 00:21:14 -0700

    v0.21.0

--- internal/flags.go
+++ internal/flags.go
@@ -86,7 +86,7 @@ func NewApp() (app *cli.App) {
 
 	app = &cli.App{
 		Name:     "goofys",
-		Version:  "0.20.0-" + VersionHash,
+		Version:  "0.21.0-" + VersionHash,
 		Usage:    "Mount an S3 bucket locally",
 		HideHelp: true,
 		Writer:   os.Stderr,
