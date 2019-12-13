dfb5c8189269c3e0f5ce21bf4b78f9a09562b7b0
Bago Amirbekian <bago@databricks.com>
2019-12-12 19:44:41 -0800

    Use getuid and getgid to avoid USER HOME environment issue
    user.Current() fails when USER & HOME are not set.

--- internal/perms.go
+++ internal/perms.go
@@ -17,34 +17,10 @@
 package internal
 
 import (
-	"os/user"
-	"strconv"
+	"os"
 )
 
 // MyUserAndGroup returns the UID and GID of this process.
-func MyUserAndGroup() (uid int, gid int) {
-	// Ask for the current user.
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-
-	// Parse UID.
-	uid64, err := strconv.ParseInt(user.Uid, 10, 32)
-	if err != nil {
-		log.Fatalf("Parsing UID (%s): %v", user.Uid, err)
-		return
-	}
-
-	// Parse GID.
-	gid64, err := strconv.ParseInt(user.Gid, 10, 32)
-	if err != nil {
-		log.Fatalf("Parsing GID (%s): %v", user.Gid, err)
-		return
-	}
-
-	uid = int(uid64)
-	gid = int(gid64)
-
-	return
+func MyUserAndGroup() (int, int) {
+	return os.Getuid(), os.Getgid()
 }
