8544071da105abb3c8d90ad35dcbd63f44c0b37e
dale0525 <logictan89@gmail.com>
2022-09-28 10:49:51 +0800

    Update api.go

--- api/api.go
+++ api/api.go
@@ -1,8 +1,8 @@
 package goofys
 
 import (
-	. "github.com/kahing/goofys/api/common"
-	"github.com/kahing/goofys/internal"
+	. "github.com/dale0525/goofys/api/common"
+	"github.com/dale0525/goofys/internal"
 
 	"context"
 	"fmt"
