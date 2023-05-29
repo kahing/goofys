358f7741f1df9b86f02b1a57840fe3652c6a2f03
jiakai <jia.kai66@gmail.com>
2023-05-29 14:56:11 -0400

    modify version info

--- main.go
+++ main.go
@@ -134,7 +134,7 @@ func massageArg0() {
 var Version = "use `make build' to fill version hash correctly"
 
 func main() {
-	VersionNumber = "0.24.0"
+	VersionNumber = "0.24.0-blkread"
 	VersionHash = Version
 
 	massagePath()
