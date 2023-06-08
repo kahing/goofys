5c04901692289346de2a70a48e72dbdba88715ad
tomaszduda23 <tomaszduda23@gmail.com>
2023-06-08 18:48:44 +0000

    add config

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,6 @@ RUN apt update; apt install -y make git golang-go
 RUN make build
 FROM ubuntu:22.04
 COPY --from=builder /build/goofys /usr/local/bin
+COPY run.sh /
+RUN chmod +x /run.sh
+CMD [/run.sh]
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p ~/.aws
+if [[ ! -z "${AWS_ACCESS_KEY_ID}" ]] && [[ ! -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
+cat <<EOF > ~/.aws/credentials
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+fi
+
+exec goofys -f --endpoint $S3_URL $OPTION $S3_BUCKET $MNT_POINT
