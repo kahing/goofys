379c660ab8c5c8675ab8a40f33a839a6b0d1f352
tomaszduda23 <tomaszduda23@gmail.com>
2023-06-08 22:48:58 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,4 +11,15 @@ fi
 
 trap "umount ${MNT_POINT}" INT TERM
 
-goofys -f --endpoint $S3_URL $OPTION $S3_BUCKET $MNT_POINT
+ARGS=""
+if [[ ! -z "${UID}" ]]; then
+  ARGS="${ARGS} --uid ${UID}"
+fi
+if [[ ! -z "${GID}" ]]; then
+  ARGS="${ARGS} --gid ${GID}"
+fi
+if [[ ! -z "${OPTION}" ]]; then
+  ARGS="${ARGS} -o ${OPTION}"
+fi
+
+goofys -f --endpoint $S3_URL $ARGS $S3_BUCKET $MNT_POINT
